45ec4f0a7c4197eda9bdbe125620eeddc0ec44a0
abhinna1 <manandharabhinna@gmail.com>
2024-02-04 13:09:42 +0545

    footer route updated to repository. / favicon and page title updated.

--- src/App.js
+++ src/App.js
@@ -14,11 +14,11 @@ function App() {
       <div className="watermark flex items-center justify-center fixed bottom-3 gap-x-2 left-1/2 transform -translate-x-1/2 text-center text-black text-[13px] font-semibold tracking-widest">
         <img src={githubLogo} alt="" className="w-5" />
         <a
-          href="https://github.com/abhinna1"
+          href="https://github.com/abhinna1/valentines"
           target="_blank"
           rel="noopener noreferrer"
         >
-          abhinna1
+          abhinna1/valentines
         </a>
       </div>
     </>
