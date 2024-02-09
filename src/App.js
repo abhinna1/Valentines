a99bd44e5390082dd528adf505a227aa05980ba9
abhinna1 <manandharabhinna@gmail.com>
2024-02-09 19:45:23 +0545

    google analytics added.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import "./static/css/tailwind.css";
 import Form from "./Form";
 import githubLogo from "./static/images/github.png";
 import Background from "./Background";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AcceptedPage from "./AcceptedPage";
+import ReactGA from "react-ga"
+
 function App() {
   const [accepted, setAccepted] = useState(false);
+  function App() {
+    useEffect(() => {
+      ReactGA.initialize('G-1YQXLXYVNV'); // Replace with your actual ID
+    }, []); // Run once on component mount
+  
+    // ... rest of your App component
+  }
   return (
     <>
       {accepted ? <Background accepted={accepted} /> : <></>}
